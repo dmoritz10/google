5fb5da84655e83a9604f63431a23a6f30dd25775
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-05-13 12:58:53 -0700

    dan

--- photos.js
+++ photos.js
@@ -505,8 +505,10 @@ async function postData(obj) {
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "same-origin", // include, *same-origin, omit
     headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
+      "Content-Type": "application/octet-stream",
+            "X-Goog-Upload-File-Name": file.name,
+            "X-Goog-Upload-Protocol": "raw",
+            Authorization: `Bearer ${accessToken}`,
     },
     redirect: "follow", // manual, *follow, error
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
