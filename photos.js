346256a7575c927cf02b9671e2f97db94e51fd10
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-05-13 06:44:04 -0700

    dan

--- photos.js
+++ photos.js
@@ -530,9 +530,7 @@ function upload({ files, albumId, accessToken }) {
               "Access-Control-Allow-Origin": "*",
               "Referrer-Policy": "no-referrer",
               "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-              "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Origin",
-              "referrer": ""
+              "Access-Control-Allow-Headers": "Content-Type"
 
             },
           }
