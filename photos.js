c49305d010bc6ea5f4569bd0debc5fe08a2b58ca
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-05-12 17:14:08 -0700

    dana

--- photos.js
+++ photos.js
@@ -529,7 +529,8 @@ function upload({ files, albumId, accessToken }) {
               "Referrer-Policy": "no-referrer",
               "Access-Control-Allow-Origin": "*",
               "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-              "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Origin"
+              "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Origin",
+              "referrer": ""
 
             },
           }
