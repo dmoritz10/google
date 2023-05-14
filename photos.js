dffcc9b54bea4be5de06b4fbbcb0d85e3d29e298
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-05-14 09:47:52 -0700

    dan

--- photos.js
+++ photos.js
@@ -658,7 +658,7 @@ function upload({ files, albumId, accessToken }) {
     return new Promise(async (resolve, reject) => {
       console.log(e);
 
-      let params = { newMediaItems: e }
+      let params = {albumId: albumId, newMediaItems: e }
       var x = await createPhotos(params)
         console.log('x', x)
     });
