c1f980ab0cbc9af6ad5a4730dad3d1ef29b37539
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2024-09-30 20:50:38 -0700

    dan

--- photos.js
+++ photos.js
@@ -423,7 +423,7 @@ async function uploadPhotos(photoFiles) {
 
     var uploadResponse = await uploadPhoto(uParams)
 
-    console.log('upload complete', i, 'of', photoFiles.files.length, data.name, Math.round(data.size / 1048576), 'mb', data)
+    console.log('upload complete', i, 'of', photoFiles.files.length, file.name, Math.round(file.size / 1048576), 'mb', file)
 
     if (uploadResponse.status != 200) {
       console.log("uploadPhotos failed", uploadResponse);
