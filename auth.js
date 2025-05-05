c5f5782434492491f0131018890a2019a255ed4b
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2025-05-05 15:53:02 -0700

    permissions

--- auth.js
+++ auth.js
@@ -1,14 +1,15 @@
-
-
-const API_KEY   = 'AIzaSyCqyn9nW8Uo15autmsv6qGfpVenoZ9lRUY'  
-const CLI_ID    = '922727887494-n8kcnouuajm7f9es4t4l52crt0rjni7b.apps.googleusercontent.com'  
-const SCOPES    = 'https://www.googleapis.com/auth/spreadsheets https://mail.google.com https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/photoslibrary.edit.appcreateddata  https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddata  https://www.googleapis.com/auth/photoslibrary.appendonly'; // Space delimited if more than one
-const DISCOVERY = ['https://sheets.googleapis.com/$discovery/rest?version=v4',
-                   'https://gmail.googleapis.com/$discovery/rest?version=v1', 
-                   'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
-photoslibrary                   'https://www.googleapis.com/discovery/v1/apis//v1/rest'
-                  ];
-const SS        = 'Google Companion'
+const API_KEY = "AIzaSyCqyn9nW8Uo15autmsv6qGfpVenoZ9lRUY";
+const CLI_ID =
+  "922727887494-n8kcnouuajm7f9es4t4l52crt0rjni7b.apps.googleusercontent.com";
+const SCOPES =
+  "https://www.googleapis.com/auth/spreadsheets https://mail.google.com https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/photoslibrary.edit.appcreateddata  https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddata  https://www.googleapis.com/auth/photoslibrary.appendonly"; // Space delimited if more than one
+const DISCOVERY = [
+  "https://sheets.googleapis.com/$discovery/rest?version=v4",
+  "https://gmail.googleapis.com/$discovery/rest?version=v1",
+  "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+  "https://www.googleapis.com/discovery/v1/apis//v1/rest",
+];
+const SS = "Google Companion";
 
 /**
  * The google libraries are loaded, and ready for action!
@@ -17,7 +18,7 @@ function proceedAsLoaded() {
   if (Goth.recognize()) {
     Goth.onetap();
   } else {
-    gotoTab('Auth')
+    gotoTab("Auth");
     forceSignin();
   }
 }
@@ -26,25 +27,28 @@ function proceedAsLoaded() {
  * They have to correctly get through the button click / sign up flow to proceed.
  */
 function forceSignin() {
-  Goth.button('signin', {type: 'standard', size: 'large', text: 'signup_with'});
+  Goth.button("signin", {
+    type: "standard",
+    size: "large",
+    text: "signup_with",
+  });
 }
 
 function signoutEvent() {
-  document.getElementById('signin').style.display = 'block';
-  gotoTab('Auth')
+  document.getElementById("signin").style.display = "block";
+  gotoTab("Auth");
   forceSignin();
 }
 
 function revokeEvent() {
-  document.getElementById('signin').style.display = 'block';
-  Goth.revoke()
-  gotoTab('Auth')
+  document.getElementById("signin").style.display = "block";
+  Goth.revoke();
+  gotoTab("Auth");
   forceSignin();
 }
 
 function proceedAsSignedIn() {
-
-  document.getElementById('signin').style.display = 'none';
+  document.getElementById("signin").style.display = "none";
   runApp();
 }
 
@@ -53,20 +57,20 @@ function proceedAsSignedIn() {
  */
 function gothWatch(event) {
   switch (event) {
-    case 'signin':
+    case "signin":
       proceedAsSignedIn();
       break;
-    case 'revoke':
-    case 'signout': 
+    case "revoke":
+    case "signout":
       signoutEvent();
       break;
-    case 'loaded':
+    case "loaded":
       proceedAsLoaded();
       break;
-    case 'onetap_suppressed':
-      forceSignin();  // If a user bypasses onetap flows, we land them with a button.
+    case "onetap_suppressed":
+      forceSignin(); // If a user bypasses onetap flows, we land them with a button.
       break;
-    default: 
+    default:
       console.log("Well, this is a surprise!");
       console.log(event);
   }
@@ -81,36 +85,30 @@ function authorize() {
 }
 
 async function runApp() {
+  user = Goth.user();
 
-  user = Goth.user()
-
-  console.log('user', user)
+  console.log("user", user);
 
   var rtn = await getSSId(SS);
 
-  if (rtn.fileId) {spreadsheetId = rtn.fileId}
-  else {
-    await confirm('getSSId error: ' + rtn.msg);
-    window.close()
+  if (rtn.fileId) {
+    spreadsheetId = rtn.fileId;
+  } else {
+    await confirm("getSSId error: " + rtn.msg);
+    window.close();
   }
   await initialUI();
 
-  gotoTab('Home')
-    
+  gotoTab("Home");
 }
 
 async function initialUI() {
-  timerStart = new Date()
+  timerStart = new Date();
 
-    arrShts = await openShts(
-      [
-        { title: 'Settings', type: "all" }
-      ])
-    
+  arrShts = await openShts([{ title: "Settings", type: "all" }]);
 
-  console.log('initialUI', arrShts)
+  console.log("initialUI", arrShts);
 
-  arrOptions    = toObject(arrShts.Settings.vals)
-  optionsIdx    = toObjectIdx(arrShts.Settings.vals)
-
-};
+  arrOptions = toObject(arrShts.Settings.vals);
+  optionsIdx = toObjectIdx(arrShts.Settings.vals);
+}
